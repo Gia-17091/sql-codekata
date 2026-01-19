5a58f36a71559abdc8a1e7508dfabb7a9296947b
Gia-17091 <haeunk0922@gmail.com>
2026-01-20 00:12:30 +0900

    [HackerRank] 입문 - Japanese Cities' Attributes (#131)

---
+++ hackerrank/1_입문/131_Japanese_Cities'_Attributes.sql
@@ -0,0 +1,9 @@
+-- Japanese Cities' Attributes
+-- HackerRank 입문 (⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/japanese-cities-attributes/problem
+-- 작성자: 김하은
+-- 작성일: 2026. 01. 20. 00:12:29
+
+SELECT *
+FROM CITY
+WHERE CountryCode = 'JPN';
