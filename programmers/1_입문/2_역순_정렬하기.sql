2b6e58e746ad9f4916e01d2e3e9f467669455642
Gia-17091 <haeunk0922@gmail.com>
2026-01-19 22:24:47 +0900

    [프로그래머스] 입문 - 역순 정렬하기 (#2)

--- programmers/1_입문/2_역순_정렬하기.sql
+++ programmers/1_입문/2_역순_정렬하기.sql
@@ -2,7 +2,7 @@
 -- 프로그래머스 입문 (⭐)
 -- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59035
 -- 작성자: 김하은
--- 작성일: 2026. 01. 19. 22:20:49
+-- 작성일: 2026. 01. 19. 22:24:46
 
 SELECT NAME, DATETIME
 FROM ANIMAL_INS
