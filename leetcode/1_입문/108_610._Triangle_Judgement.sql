6089ba4936cc5c7c865614f5e3cb8f590f21726e
Gia-17091 <haeunk0922@gmail.com>
2026-01-19 23:17:11 +0900

    Fix triangle validation SQL query
    Corrected SQL query logic for triangle validation.

--- leetcode/1_입문/108_610._Triangle_Judgement.sql
+++ leetcode/1_입문/108_610._Triangle_Judgement.sql
@@ -4,12 +4,12 @@
 -- 작성자: 김하은
 -- 작성일: 2026. 01. 19. 23:13:09
 
-            AND y+ z >= x
-            THEN 'Yes'
-        ELSE 'No'
-            AND x+ z>= y 
-        END AS triangle
-FROM Triangle
 # Write your MySQL query statement below
 SELECT x, y, z, 
-    CASE WHEN x+y>=z 
+    CASE WHEN x+y>z
+            AND y+ z >= x
+            AND x+ z>= y 
+            THEN 'Yes'
+        ELSE 'No'
+        END AS triangle
+FROM Triangle
