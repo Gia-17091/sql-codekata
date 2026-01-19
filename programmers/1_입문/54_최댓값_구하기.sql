a0b5ae43cbc8eae575e74133d918d56f7d74c1d1
Gia-17091 <haeunk0922@gmail.com>
2026-01-19 22:28:54 +0900

    [프로그래머스] 입문 - 최댓값 구하기 (#54)

---
+++ programmers/1_입문/54_최댓값_구하기.sql
@@ -0,0 +1,8 @@
+-- 최댓값 구하기
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59415
+-- 작성자: 김하은
+-- 작성일: 2026. 01. 19. 22:28:53
+
+SELECT MAX(DATETIME) AS 시간
+FROM ANIMAL_INS
