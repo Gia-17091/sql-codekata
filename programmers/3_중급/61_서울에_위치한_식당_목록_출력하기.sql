e7b8a1dba4ffc5cd7f19af31bbf131c27bcdf863
Gia-17091 <haeunk0922@gmail.com>
2026-01-19 23:54:52 +0900

    [프로그래머스] 중급 - 서울에 위치한 식당 목록 출력하기 (#61)

---
+++ programmers/3_중급/61_서울에_위치한_식당_목록_출력하기.sql
@@ -0,0 +1,18 @@
+-- 서울에 위치한 식당 목록 출력하기
+-- 프로그래머스 중급 (⭐⭐⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/131118
+-- 작성자: 김하은
+-- 작성일: 2026. 01. 19. 23:54:51
+
+SELECT I.REST_ID,
+    I.REST_NAME,
+    I.FOOD_TYPE,
+    I.FAVORITES,
+    I.ADDRESS,
+    ROUND(AVG(R.REVIEW_SCORE),2) AS SCORE
+FROM REST_INFO I
+JOIN REST_REVIEW R
+    ON I.REST_ID = R.REST_ID
+WHERE I.ADDRESS LIKE '서울%'
+GROUP BY I.FAVORITES
+ORDER BY SCORE DESC, I.FAVORITES DESC;
