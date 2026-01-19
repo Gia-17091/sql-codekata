2d064fad6bf6752f9e1268d248412e6d3cace765
Gia-17091 <haeunk0922@gmail.com>
2026-01-19 22:26:28 +0900

    [프로그래머스] 입문 - 상위 n개 레코드 (#8)

---
+++ programmers/1_입문/8_상위_n개_레코드.sql
@@ -0,0 +1,10 @@
+-- 상위 n개 레코드
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59405
+-- 작성자: 김하은
+-- 작성일: 2026. 01. 19. 22:26:27
+
+SELECT NAME
+FROM ANIMAL_INS
+ORDER BY DATETIME
+LIMIT 1;
