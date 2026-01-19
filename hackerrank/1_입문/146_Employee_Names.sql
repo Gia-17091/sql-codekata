82d450ffe8210af73760e0ac1924542692eb6c5a
Gia-17091 <haeunk0922@gmail.com>
2026-01-20 00:18:47 +0900

    [HackerRank] 입문 - Employee Names (#146)

---
+++ hackerrank/1_입문/146_Employee_Names.sql
@@ -0,0 +1,9 @@
+-- Employee Names
+-- HackerRank 입문 (⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/name-of-employees/problem
+-- 작성자: 김하은
+-- 작성일: 2026. 01. 20. 00:18:46
+
+SELECT name
+FROM Employee
+ORDER BY NAME ASC;
