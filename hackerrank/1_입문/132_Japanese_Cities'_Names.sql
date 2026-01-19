42a686a4ff8d3b392a17b0a374300a99bb985c95
Gia-17091 <haeunk0922@gmail.com>
2026-01-20 00:13:50 +0900

    [HackerRank] 입문 - Japanese Cities' Names (#132)

---
+++ hackerrank/1_입문/132_Japanese_Cities'_Names.sql
@@ -0,0 +1,9 @@
+-- Japanese Cities' Names
+-- HackerRank 입문 (⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/japanese-cities-name/problem
+-- 작성자: 김하은
+-- 작성일: 2026. 01. 20. 00:13:49
+
+SELECT  name
+FROM CITY
+WHERE CountryCode = 'JPN';
