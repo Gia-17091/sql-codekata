5adbefeeacee5411422d2e90897bdd2cdfd636a9
Gia-17091 <haeunk0922@gmail.com>
2026-01-20 00:11:53 +0900

    [HackerRank] 입문 - Select By ID (#130)

---
+++ hackerrank/1_입문/130_Select_By_ID.sql
@@ -0,0 +1,9 @@
+-- Select By ID
+-- HackerRank 입문 (⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/select-by-id/problem
+-- 작성자: 김하은
+-- 작성일: 2026. 01. 20. 00:11:52
+
+SELECT *
+FROM CITY
+WHERE id ='1661'
