fd9aa919218864cadf542f81b62b038d0b332f37
Gia-17091 <haeunk0922@gmail.com>
2026-01-20 09:30:18 +0900

    [HackerRank] 입문 - Revising Aggregations - The Count Function (#150)

---
+++ hackerrank/1_입문/150_Revising_Aggregations_-_The_Count_Function.sql
@@ -0,0 +1,9 @@
+-- Revising Aggregations - The Count Function
+-- HackerRank 입문 (⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem
+-- 작성자: 김하은
+-- 작성일: 2026. 01. 20. 09:30:17
+
+SELECT COUNT(*)
+FROM CITY
+WHERE POPULATION > 100000
