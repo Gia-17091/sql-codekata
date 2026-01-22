06b15da5f57a15f099fadb312524dda5f26ee1de
Gia-17091 <haeunk0922@gmail.com>
2026-01-22 21:59:59 +0900

    [HackerRank] 입문 - Revising Aggregations - The Sum Function (#151)

---
+++ hackerrank/1_입문/151_Revising_Aggregations_-_The_Sum_Function.sql
@@ -0,0 +1,9 @@
+-- Revising Aggregations - The Sum Function
+-- HackerRank 입문 (⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/revising-aggregations-sum/problem
+-- 작성자: 김하은
+-- 작성일: 2026. 01. 22. 21:59:57
+
+SELECT SUM(POPULATION)
+FROM CITY
+WHERE DISTRICT = 'California';
