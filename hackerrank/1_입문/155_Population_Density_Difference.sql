a65be6b7b22b2d099b9c91d1f217c26eeee7089d
Gia-17091 <haeunk0922@gmail.com>
2026-01-23 09:19:41 +0900

    [HackerRank] 입문 - Population Density Difference (#155)

---
+++ hackerrank/1_입문/155_Population_Density_Difference.sql
@@ -0,0 +1,8 @@
+-- Population Density Difference
+-- HackerRank 입문 (⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/population-density-difference/problem
+-- 작성자: 김하은
+-- 작성일: 2026. 01. 23. 09:19:39
+
+SELECT MAX(POPULATION)-MIN(POPULATION)
+FROM CITY
