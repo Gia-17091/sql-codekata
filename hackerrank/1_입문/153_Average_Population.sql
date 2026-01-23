f56a770b3f3369fad8e70d1f8fd56b8372f223d1
Gia-17091 <haeunk0922@gmail.com>
2026-01-23 09:12:55 +0900

    [HackerRank] 입문 - Average Population (#153)

---
+++ hackerrank/1_입문/153_Average_Population.sql
@@ -0,0 +1,8 @@
+-- Average Population
+-- HackerRank 입문 (⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/average-population/problem
+-- 작성자: 김하은
+-- 작성일: 2026. 01. 23. 09:12:53
+
+SELECT ROUND(AVG(POPULATION))
+FROM CITY;
