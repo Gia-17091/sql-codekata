6064e81003903acaed28ee6c7a25db2208ca577c
Gia-17091 <haeunk0922@gmail.com>
2026-01-26 09:46:15 +0900

    [LeetCode] 기초 - 1729. Find Followers Count (#103)

---
+++ leetcode/2_기초/103_1729._Find_Followers_Count.sql
@@ -0,0 +1,11 @@
+-- 1729. Find Followers Count
+-- LeetCode 기초 (⭐⭐)
+-- 문제 링크: https://leetcode.com/problems/find-followers-count/
+-- 작성자: 김하은
+-- 작성일: 2026. 01. 26. 09:46:14
+
+SELECT user_id, 
+    COUNT(user_id) AS followers_count
+GROUP BY user_id
+FROM Followers
+ORDER BY user_id
