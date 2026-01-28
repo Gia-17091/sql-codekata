18767833c2145540b1dfac72a26c6bf0cd8d4271
Gia-17091 <haeunk0922@gmail.com>
2026-01-28 09:18:55 +0900

    [HackerRank] 기초 - Weather Observation Station 4 (#136)

---
+++ hackerrank/2_기초/136_Weather_Observation_Station_4.sql
@@ -0,0 +1,8 @@
+-- Weather Observation Station 4
+-- HackerRank 기초 (⭐⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/weather-observation-station-4/problem
+-- 작성자: 김하은
+-- 작성일: 2026. 01. 28. 09:18:54
+
+SELECT COUNT(CITY) - COUNT(DISTINCT CITY)
+FROM STATION
