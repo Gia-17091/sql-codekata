1164901f9b02fc822a805859bb12835a7690f696
Gia-17091 <haeunk0922@gmail.com>
2026-01-28 09:28:13 +0900

    [HackerRank] 기초 - Weather Observation Station 8 (#140)

---
+++ hackerrank/2_기초/140_Weather_Observation_Station_8.sql
@@ -0,0 +1,12 @@
+-- Weather Observation Station 8
+-- HackerRank 기초 (⭐⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/weather-observation-station-8/problem
+-- 작성자: 김하은
+-- 작성일: 2026. 01. 28. 09:28:12
+
+    CITY LIKE '%a' OR
+    CITY LIKE '%e' OR
+    CITY LIKE '%i' OR
+    CITY LIKE '%o' OR
+    CITY LIKE '%u'
+);
