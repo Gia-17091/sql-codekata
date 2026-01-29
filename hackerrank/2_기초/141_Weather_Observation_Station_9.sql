521c4c75b7685d787c08ee2cd5e3d887981c55d9
Gia-17091 <haeunk0922@gmail.com>
2026-01-29 11:38:29 +0900

    [HackerRank] 기초 - Weather Observation Station 9 (#141)

---
+++ hackerrank/2_기초/141_Weather_Observation_Station_9.sql
@@ -0,0 +1,9 @@
+-- Weather Observation Station 9
+-- HackerRank 기초 (⭐⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/weather-observation-station-9/problem
+-- 작성자: 김하은
+-- 작성일: 2026. 01. 29. 11:38:27
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE SUBSTR(CITY,1,1) NOT IN ('A','E','I','O','U')
