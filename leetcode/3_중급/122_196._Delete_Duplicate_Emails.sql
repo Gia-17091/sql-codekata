6f22bdfd734fbbe637aeb8a64fcd0d504180c9ca
Gia-17091 <haeunk0922@gmail.com>
2026-02-06 02:03:59 +0900

    Refactor delete duplicate emails SQL query
    Reformatted SQL delete statement for better readability and added example for deleting a user.

--- leetcode/3_중급/122_196._Delete_Duplicate_Emails.sql
+++ leetcode/3_중급/122_196._Delete_Duplicate_Emails.sql
@@ -4,9 +4,14 @@
 -- 작성자: 김하은
 -- 작성일: 2026. 02. 06. 01:27:04
 
-DELETE p1
-FROM Person p1
-JOIN Person p2
-  ON p1.email = p2.email
- AND p1.id > p2.id;
-
+DELETE p1
+FROM Person p1
+JOIN Person p2
+  ON p1.email = p2.email
+ AND p1.id > p2.id;
+ 
+'''
+DELETE FROM 테이블명 WHERE 조건;
+-- 예: users 테이블에서 id가 10인 회원 삭제
+＃DELETE FROM users WHERE id = 10;
+'''
