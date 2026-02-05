d1d97e35f75c57938d91866016c10cdd414d6462
Gia-17091 <haeunk0922@gmail.com>
2026-02-06 02:05:25 +0900

    Refactor SQL query to select second highest salary

--- leetcode/3_중급/123_176._Second_Highest_Salary.sql
+++ leetcode/3_중급/123_176._Second_Highest_Salary.sql
@@ -4,10 +4,9 @@
 -- 작성자: 김하은
 -- 작성일: 2026. 02. 06. 01:47:11
 
-    SELECT DISTINCT salary
-) AS SecondHighestSalary;
-SELECT (
-    FROM Employee
-    ORDER BY salary DESC
-
-    LIMIT 1 OFFSET 1
+SELECT (
+    SELECT DISTINCT salary
+    FROM Employee
+    ORDER BY salary DESC
+    LIMIT 1 OFFSET 1       #OFFSET n → 앞에서 n개를 스킵
+) AS SecondHighestSalary;
