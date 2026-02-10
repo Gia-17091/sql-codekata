9639fb8e6ae0dcb45dd4ebd116c1ce79d04d2e2b
Gia-17091 <haeunk0922@gmail.com>
2026-02-10 20:56:36 +0900

    [HackerRank] 중급 - Type of Triangle (#148)

---
+++ hackerrank/3_중급/148_Type_of_Triangle.sql
@@ -0,0 +1,7 @@
+-- Type of Triangle
+-- HackerRank 중급 (⭐⭐⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/what-type-of-triangle/problem
+-- 작성자: 김하은
+-- 작성일: 2026. 02. 10. 20:56:35
+
+FROM TRIANGLES
