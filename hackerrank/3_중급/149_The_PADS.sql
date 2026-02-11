3c7fc24f667546d82aad48f90bab975aa4827759
Gia-17091 <haeunk0922@gmail.com>
2026-02-11 09:30:39 +0900

    [HackerRank] 중급 - The PADS (#149)

---
+++ hackerrank/3_중급/149_The_PADS.sql
@@ -0,0 +1,9 @@
+-- The PADS
+-- HackerRank 중급 (⭐⭐⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/the-pads/problem
+-- 작성자: 김하은
+-- 작성일: 2026. 02. 11. 09:30:38
+
+FROM OCCUPATIONS
+GROUP BY OCCUPATION
+ORDER BY COUNT(*), OCCUPATION;
