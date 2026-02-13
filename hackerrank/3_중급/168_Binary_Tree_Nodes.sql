d0bae31cf28a48f8dfdfd73f756bf484a944f76d
Gia-17091 <haeunk0922@gmail.com>
2026-02-13 12:00:13 +0900

    [HackerRank] 중급 - Binary Tree Nodes (#168)

---
+++ hackerrank/3_중급/168_Binary_Tree_Nodes.sql
@@ -0,0 +1,8 @@
+-- Binary Tree Nodes
+-- HackerRank 중급 (⭐⭐⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/binary-search-tree-1/problem
+-- 작성자: 김하은
+-- 작성일: 2026. 02. 13. 12:00:11
+
+FROM BST b
+ORDER BY b.N
