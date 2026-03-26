f9ba305459aacf4ee2ef613390ea78d2546c868c
Gia-17091 <haeunk0922@gmail.com>
2026-03-26 10:08:50 +0900

    [HackerRank] 고급 - Draw The Triangle 2 (#182)

---
+++ hackerrank/4_고급/182_Draw_The_Triangle_2.sql
@@ -0,0 +1,7 @@
+-- Draw The Triangle 2
+-- HackerRank 고급 (⭐⭐⭐⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/draw-the-triangle-2/problem
+-- 작성자: 김하은
+-- 작성일: 2026. 03. 26. 10:08:49
+
+ORDER BY 1 ASC;
