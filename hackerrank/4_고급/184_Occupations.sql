cf0834f26c96ae7c134962cd72987516e80344a3
Gia-17091 <haeunk0922@gmail.com>
2026-03-27 07:47:06 +0900

    [HackerRank] 고급 - Occupations (#184)

--- hackerrank/4_고급/184_Occupations.sql
+++ hackerrank/4_고급/184_Occupations.sql
@@ -2,12 +2,14 @@
 -- HackerRank 고급 (⭐⭐⭐⭐)
 -- 문제 링크: https://www.hackerrank.com/challenges/occupations/problem
 -- 작성자: 김하은
--- 작성일: 2026. 03. 27. 07:46:20
+-- 작성일: 2026. 03. 27. 07:47:04
 
-AS Doctor,
-    MAX(CASE WHEN Occupation = 'Professor' THEN Name END) AS Professor,
-    MAX(CASE WHEN Occupation = 'Singer' THEN Name END) AS Singer,
-    MAX(CASE WHEN Occupation = 'Actor' THEN Name END) AS Actor
-FROM CTE
-GROUP BY rn
-ORDER BY rn;
+WITH CTE AS (
+    SELECT 
+        Name,
+        Occupation,
+        ROW_NUMBER() OVER (PARTITION BY Occupation ORDER BY Name) AS rn
+    FROM OCCUPATIONS
+)
+SELECT 
+    MAX(CASE WHEN Occupation = 'Doctor' THEN Name END) 
